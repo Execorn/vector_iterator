a4dc546159960986d9e992728455f42a8269e3f4
execorn <n.biryukov.04@gmail.com>
2023-03-21 00:22:36 +0300

    AllocPolicy/ObjectTraits added

--- src/allocators/pool/pool_alloc.hpp
+++ src/allocators/pool/pool_alloc.hpp
@@ -14,7 +14,9 @@ namespace X17 {
 
 template <typename T>
 class ObjectTraits {
+    /* TYPEDEFS */
     using value_type = T;
+    /* END OF TYPEDEFS */
 
    public:
     template <typename U>
@@ -33,10 +35,63 @@ class ObjectTraits {
 
     inline value_type const* address(value_type const& ref) { return &ref; }
 
-    inline void construct(value_type* mem_ptr, const value_type& initial_value) { new (mem_ptr) value_type(initial_value); }
+    inline void construct(value_type* mem_ptr,
+                          const value_type& initial_value) {
+        new (mem_ptr) value_type(initial_value);
+    }
 
     inline void destroy(value_type* mem_ptr) { mem_ptr->~value_type(); }
 };
+
+template <typename T>
+class StandardAllocPolicy {
+   public:
+    //    typedefs
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+
+   public:
+    //    convert an StandardAllocPolicy<T> to StandardAllocPolicy<U>
+    template <typename U>
+    struct rebind {
+        typedef StandardAllocPolicy<U> other;
+    };
+
+   public:
+    inline explicit StandardAllocPolicy() {}
+    inline ~StandardAllocPolicy() {}
+    
+    inline explicit StandardAllocPolicy(StandardAllocPolicy const&) {}
+
+    template <typename U>
+    inline explicit StandardAllocPolicy(StandardAllocPolicy<U> const&) {}
+
+    inline pointer allocate(size_type cnt,
+                            typename std::allocator<void>::const_pointer = 0) {
+        return reinterpret_cast<pointer>(::operator new(cnt * sizeof(T)));
+    }
+
+    inline void deallocate(pointer p, size_type) { ::operator delete(p); }
+
+    inline size_type max_size() const {
+        return std::numeric_limits<size_type>::max();
+    }
+};
+
+template <typename T, typename T2>
+inline bool operator==(StandardAllocPolicy<T> const&,
+                       StandardAllocPolicy<T2> const&) {
+    return true;
+}
+template <typename T, typename OtherAllocator>
+inline bool operator==(StandardAllocPolicy<T> const&, OtherAllocator const&) {
+    return false;
+}
 
 template <typename T>
 class PoolAllocator {
