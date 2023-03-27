2f283140be689c4758d2f3329e9a9fe5c192f0ce
execorn <n.biryukov.04@gmail.com>
2023-03-27 09:34:19 +0300

    config added

---
+++ src/allocators/pool/config_pool.hpp
@@ -0,0 +1,17 @@
+/* X17::PoolAllocator CONFIGURATION FILE */
+
+// ---------------------------------------------------------
+
+/* uncomment this line for enabling DEBUG mode */
+// #define POOL_ALLOC_DEBUG
+
+// ---------------------------------------------------------
+
+/* set chunks-per-block value for allocator */
+/* comment this line to enable default mode */
+/* default mode : 1024 chunks per block*/
+#define DEFAULT_CHUNKS_PER_BLOCK 1024
+
+// ---------------------------------------------------------
+
+/* DO NOT EDIT ANYTHING AFTER THIS LINE*/
